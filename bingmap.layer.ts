e09a010cb0f5a42d293da2dc68506192855de363
Nguyen Van Tan <exfick@gmail.com>
2022-03-28 14:08:56 +0700

    bing layer

--- bingmap.layer.ts
+++ bingmap.layer.ts
@@ -1,99 +1,75 @@
-import { TileLayer, TileLayerOptions } from 'leaflet';
 import * as L from 'leaflet';
+import { TileLayer, TileLayerOptions } from 'leaflet';
 
+export enum VALID_IMAGERY_SETS {
+  'Aerial' = 'Aerial',
+  'AerialWithLabels' = 'AerialWithLabels',
+  'AerialWithLabelsOnDemand' = 'AerialWithLabelsOnDemand',
+  'Road' = 'Road',
+  'RoadOnDemand' = 'RoadOnDemand',
+  'CanvasLight' = 'CanvasLight',
+  'CanvasDark' = 'CanvasDark',
+  'CanvasGray' = 'CanvasGray',
+  'OrdnanceSurvey' = 'OrdnanceSurvey',
+}
+export enum DYNAMIC_IMAGERY_SETS {
+  'AerialWithLabelsOnDemand' = 'AerialWithLabelsOnDemand',
+  'RoadOnDemand' = 'RoadOnDemand',
+}
 export interface BingOptions extends TileLayerOptions {
-  bingMapsKey?: string,
-  imagerySet?: string,
-  culture?: "en-US",
-  minZoom?: 1,
-  minNativeZoom?: 1,
-  maxNativeZoom?: 19,
-  style?: string,
-  visible?: boolean,
+  bingMapsKey: string;
+  imagerySet?: VALID_IMAGERY_SETS;
+  culture?: 'en-US';
+  minZoom?: 1;
+  minNativeZoom?: 1;
+  maxNativeZoom?: 19;
+  style?: DYNAMIC_IMAGERY_SETS;
+  visible?: boolean;
 }
 
-// 'AuUhEkxn8U72sGMKcUR_8mMhD5f882wua87imHOQ9HWl06K-svQjWTGEJAbEPBg6'
 export class BingLayer extends TileLayer {
   options: BingOptions;
 
-  static METADATA_URL = "https://dev.virtualearth.net/REST/v1/Imagery/Metadata/Aerial?key=AuUhEkxn8U72sGMKcUR_8mMhD5f882wua87imHOQ9HWl06K-svQjWTGEJAbEPBg6&include=ImageryProviders&uriScheme=https";
-  static POINT_METADATA_URL = "https://dev.virtualearth.net/REST/v1/Imagery/Metadata/Aerial/{lat},{lng}?zl={z}&key=AuUhEkxn8U72sGMKcUR_8mMhD5f882wua87imHOQ9HWl06K-svQjWTGEJAbEPBg6&uriScheme=https";
+  static METADATA_URL =
+    'https://dev.virtualearth.net/REST/v1/Imagery/Metadata/Aerial?key=AuUhEkxn8U72sGMKcUR_8mMhD5f882wua87imHOQ9HWl06K-svQjWTGEJAbEPBg6&include=ImageryProviders&uriScheme=https';
+  static POINT_METADATA_URL =
+    'https://dev.virtualearth.net/REST/v1/Imagery/Metadata/Aerial/{lat},{lng}?zl={z}&key=AuUhEkxn8U72sGMKcUR_8mMhD5f882wua87imHOQ9HWl06K-svQjWTGEJAbEPBg6&uriScheme=https';
 
-  fetchJsonp = require("fetch-jsonp");
-  _url: string;
   _imageryProviders: [];
-  _fetch;
   _getSubdomain;
   _attributions: [];
   _updateAttribution;
-  _onTileRemove
-
-   VALID_IMAGERY_SETS = [
-    'Aerial',
-    'AerialWithLabels',
-    'AerialWithLabelsOnDemand',
-    'Road',
-    'RoadOnDemand',
-    'CanvasLight',
-    'CanvasDark',
-    'CanvasGray',
-    'OrdnanceSurvey'
-  ]
-  
-   DYNAMIC_IMAGERY_SETS = [
-    'AerialWithLabelsOnDemand',
-    'RoadOnDemand'
-  ]
+  _onTileRemove;
+  _url: string;
 
   constructor(options: BingOptions) {
+    super(null, options);
 
-    super(`https://www.bing.com/api/maps/mapcontrol?callback=GetMap&key=${options.bingMapsKey}`, options)
+    options = L.Util.setOptions(this, options);
 
+    this._imageryProviders = [];
+    this._attributions = [];
 
-    if (typeof options === 'string') {
-      options = { bingMapsKey: options }
-    }
-    if (options && options.bingMapsKey) {
-      options.bingMapsKey = options.bingMapsKey
-      console.warn('use options.bingMapsKey instead of options.BingMapsKey')
-    }
-    if (!options || !options.bingMapsKey) {
-      throw new Error('Must supply options.BingMapsKey')
-    }
-    options = L.Util.setOptions(this, options)
-    if (this.VALID_IMAGERY_SETS.indexOf(options.imagerySet) < 0) {
-      throw new Error("'" + options.imagerySet + "' is an invalid imagerySet, see https://github.com/digidem/leaflet-bing-layer#parameters")
-    }
-    if (options && options.style && this.DYNAMIC_IMAGERY_SETS.indexOf(options.imagerySet) < 0) {
-      console.warn('Dynamic styles will only work with these imagerySet choices: ' + this.DYNAMIC_IMAGERY_SETS.join(', '))
-    }
-
-    var metaDataUrl = L.Util.template(BingLayer.METADATA_URL, {
-      bingMapsKey: this.options.bingMapsKey,
-      imagerySet: this.options.imagerySet
-    })
-
-    this._imageryProviders = []
-    this._attributions = []
-
-    // Keep a reference to the promise so we can use it later
-    this._fetch = this.fetchJsonp(metaDataUrl, {jsonpCallback: 'jsonp'})
-      .then(function (response) {
-        return response.json()
-      })
-      .then(this._metaDataOnLoad.bind(this))
-      .catch(console.error.bind(console))
-
-    // for https://github.com/Leaflet/Leaflet/issues/137
     if (!L.Browser.android) {
-      this.on('tileunload', this._onTileRemove)
+      this.on('tileunload', this._onTileRemove);
     }
-
   }
 
+  _fetch() {
+    var metaDataUrl = L.Util.template(BingLayer.METADATA_URL, {
+      bingMapsKey: this.options.bingMapsKey,
+      imagerySet: this.options.imagerySet,
+    });
+    return fetch(metaDataUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(this._metaDataOnLoad.bind(this))
+      .catch(console.error.bind(console));
+  }
 
   toQuadKey(x, y, z) {
-    var index = "";
+    var index = '';
     for (var i = z; i > 0; i--) {
       var b = 0;
       var mask = 1 << (i - 1);
@@ -105,36 +81,38 @@ export class BingLayer extends TileLayer {
   }
 
   toBingBBox(bboxString) {
-    var bbox = bboxString.split(",");
+    var bbox = bboxString.split(',');
     return [bbox[1], bbox[0], bbox[3], bbox[2]];
   }
 
   createTile(coords, done) {
-    var tile = document.createElement("img");
+    var tile = document.createElement('img');
 
     L.DomEvent.on(
       tile,
-      "load",
+      'load',
       L.Util.bind(this._tileOnLoad, this, done, tile)
     );
     L.DomEvent.on(
       tile,
-      "error",
+      'error',
       L.Util.bind(this._tileOnError, this, done, tile)
     );
 
     if (this.options.crossOrigin) {
-      tile.crossOrigin = "";
+      tile.crossOrigin = '';
     }
-    tile.alt = "";
+    tile.alt = '';
     // Don't create closure if we don't have to
     if (this._url) {
       tile.src = this.getTileUrl(coords);
     } else {
-      this._fetch.then(function () {
-        tile.src = this.getTileUrl(coords);
-      }.bind(this)
-      )
+      this._fetch()
+        .then(
+          function () {
+            tile.src = this.getTileUrl(coords);
+          }.bind(this)
+        )
         .catch((e) => {
           console.error(e);
           done(e);
@@ -150,8 +128,8 @@ export class BingLayer extends TileLayer {
       subdomain: this._getSubdomain(coords),
       culture: this.options.culture,
     });
-    if (typeof this.options.style === "string") {
-      url += "&st=" + this.options.style;
+    if (typeof this.options.style === 'string') {
+      url += '&st=' + this.options.style;
     }
     return url;
   }
@@ -170,7 +148,7 @@ export class BingLayer extends TileLayer {
 
   // Update the attribution control every time the map is moved
   onAddBingLayer(map: L.Map) {
-    map.on("moveend", this._updateAttribution, this);
+    map.on('moveend', this._updateAttribution, this);
     L.TileLayer.prototype.onAdd.call(this, map);
     this._attributions.forEach((attribution) => {
       map.attributionControl.addAttribution(attribution);
@@ -179,45 +157,17 @@ export class BingLayer extends TileLayer {
 
   // Clean up events and remove attributions from attribution control
   onRemoveBingLayer(map: L.Map) {
-    map.off("moveend", this._updateAttribution, this);
+    map.off('moveend', this._updateAttribution, this);
     this._attributions.forEach((attribution) => {
       map.attributionControl.removeAttribution(attribution);
     });
     L.TileLayer.prototype.onRemove.call(this, map);
   }
 
-  getMetaData(latlng, zoom) {
-    if (!this._map && (!latlng || !zoom)) {
-      return Promise.reject(
-        new Error(
-          "If layer is not attached to map, you must provide LatLng and zoom"
-        )
-      );
-    }
-    latlng = latlng || this._map.getCenter();
-    zoom = zoom || this._map.getZoom();
-    var PointMetaDataUrl = L.Util.template(
-      BingLayer.POINT_METADATA_URL,
-      {
-        bingMapsKey: this.options.bingMapsKey,
-        imagerySet: this.options.imagerySet,
-        z: zoom,
-        lat: latlng.lat,
-        lng: latlng.lng,
-      }
-    );
-    return this.fetchJsonp(PointMetaDataUrl, { jsonpCallback: "jsonp" })
-      .then((response) => {
-        return response.json();
-      })
-      .catch(console.error.bind(console));
-  }
-
   _metaDataOnLoad(metaData) {
-    if (metaData.statusCode !== 200) {
+    if (metaData.statusCode !== 200 && metaData.statusCode !== 429) {
       throw new Error(
-        "Bing Imagery Metadata error: \n" +
-        JSON.stringify(metaData, null, "  ")
+        'Bing Imagery Metadata error: \n' + JSON.stringify(metaData, null, '  ')
       );
     }
     var resource = metaData.resourceSets[0].resources[0];
@@ -227,5 +177,4 @@ export class BingLayer extends TileLayer {
     this._updateAttribution();
     return Promise.resolve();
   }
-
 }
